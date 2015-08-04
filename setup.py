c3f8bce420e4d6f2bb02114d99a98bcf6e7bd4f7
Jay Luker <lbjay@reallywow.com>
2015-08-04 05:45:43 -0400

    switch to pytest; run tests via `python setup.py test`

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 import os
 import re
+import sys
+import pytest
 import codecs
 from setuptools import setup, find_packages
+
+from setuptools.command.test import test as TestCommand
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -14,13 +32,13 @@ version_file = read('pyhorn/__init__.py')
 version = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M).group(1)
 
 install_requires = ["requests", "requests-cache"]
-tests_require = ["nose", "httmock"]
+tests_require = ["pytest", "httmock"]
 
 setup(
     name='pyhorn',
     version=version,
     packages=find_packages(exclude=["docs", "tests*"]),
-    url='https://github.com/harvard-dce/pyhorn',
+    url='https://bitbucket.org/lbjay/pyhorn',
     license='Apache 2.0',
     author='Jay Luker',
     author_email='lbjay@reallywow.com',
@@ -28,7 +46,7 @@ setup(
     long_description=readme + '\n\n' + history,
     install_requires=install_requires,
     tests_require=tests_require,
-    zip_safe=True,
+    cmdclass={'test': PyTest},
     classifiers=(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
